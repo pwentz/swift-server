6376b4e9d1191422800d8c95f68b967d607387e0
pwentz <patrick.w.wentz@gmail.com>
2017-03-16 15:38:40 -0500

    HOTFIX - replace Vapor dependencies with SocksCore submodule.

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
       ])
     ],
     dependencies: [
-      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+      .Package(url: "https://github.com/vapor/socks.git", majorVersion: 1, minor: 2)
     ]
 )
