1250da2f47c1d27d9f7ca0dcc0135b389aadbb80
pwentz <patrick.w.wentz@gmail.com>
2017-03-16 09:08:35 -0500

    FileWriter class to abstract writing log data.

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -13,25 +13,16 @@ let formatter = DateFormatter()
 formatter.dateFormat = "MM-dd-yyyy"
 
 let fileName = "logs-\(hour):\(minutes):\(seconds)--" + formatter.string(from: today)
-
 let contents = CommandLine.arguments
+let fullDirPath = contents.first!.components(separatedBy: "/Users/").last!.components(separatedBy: "/.build").first! + "/Sources/Server/Debug"
 
-let fullDirPath = contents.first!.components(separatedBy: "/Users/").last!.components(separatedBy: "/.build").first!
-
-let DocumentDirURL = try! FileManager.default.url(for: .userDirectory, in: .allDomainsMask, appropriateFor: nil, create: true)
-let logsFileUrl = DocumentDirURL.appendingPathComponent("\(fullDirPath)/Sources/Server/Debug/\(fileName)").appendingPathExtension("txt")
+FileWriter(at: fullDirPath, with: contents.joined(separator: "\r\n")).write(to: fileName)
 
 if let directoryFlagIndex = contents.index(where: { $0 == "-d" }) {
   let pathIndex = contents.index(after: directoryFlagIndex)
   let directoryPath: String = contents[pathIndex]
 
   print("||||||", FileReader(at: directoryPath).readContents())
-}
-
-do {
-  try contents.first!.write(to: logsFileUrl, atomically: true, encoding: String.Encoding.utf8)
-} catch let error as NSError {
-  print("ERROR:", error)
 }
 
 let router = Router()
