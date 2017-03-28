dfff868da89e89bfaa000b031054fc094d91eeaf
pwentz <patrick.w.wentz@gmail.com>
2017-03-28 14:09:47 -0500

    Feature/method_options2: - ternary to MethodOptionsController to determine which verbs to allow, - additional statement in ControllerFactory to send both method_options routes to MethodOptionsController.

--- Tests/ControllersTests/MethodOptionsControllerTest.swift
+++ Tests/ControllersTests/MethodOptionsControllerTest.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import Controllers
 
 class MethodOptionsControllerTest: XCTestCase {
-  func testItCanRespondWithStatus200() {
+  func testItCanRespondWithStatus200OnMethodOptionsRequest() {
     let rawRequest = "OPTIONS /method_options HTTP/1.1\r\n Host: localhost:5000\r\n Connection: Keep-Alive\r\n User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\r\n Accept-Encoding: gzip,deflate"
     let request = Request(for: rawRequest)
 
@@ -22,4 +22,25 @@ class MethodOptionsControllerTest: XCTestCase {
 
     XCTAssertEqual(response.headers["Allow"]!, expected)
   }
+
+  func testItCanRespondWithStatus200MethodOptions2Request() {
+    let rawRequest = "OPTIONS /method_options2 HTTP/1.1\r\n Host: localhost:5000\r\n Connection: Keep-Alive\r\n User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\r\n Accept-Encoding: gzip,deflate"
+    let request = Request(for: rawRequest)
+
+    let response = MethodOptionsController().process(request)
+
+    XCTAssertEqual(response.statusCode, "200 OK")
+  }
+
+  func testItCanRespondWithCorrectHeadersMethodOptions2Request() {
+    let rawRequest = "OPTIONS /method_options2 HTTP/1.1\r\n Host: localhost:5000\r\n Connection: Keep-Alive\r\n User-Agent: Apache-HttpClient/4.3.5 (java 1.5)\r\n Accept-Encoding: gzip,deflate"
+    let request = Request(for: rawRequest)
+
+    let response = MethodOptionsController().process(request)
+
+    let expected = "GET,OPTIONS"
+
+    XCTAssertEqual(response.headers["Allow"], expected)
+  }
+
 }
